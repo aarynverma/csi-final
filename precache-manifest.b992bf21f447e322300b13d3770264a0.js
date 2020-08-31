9f4633484a40baf1e29228962619c46a57114387
aarynverma <18100BTCSE02667@svvvindore.onmicrosoft.com>
2020-08-31 19:39:49 +0530

    Updates

---
+++ precache-manifest.b992bf21f447e322300b13d3770264a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a7af20b3c573836fee70d65d2ee019",
+    "url": "/csi-final/index.html"
+  },
+  {
+    "revision": "fac74be35372251d4956",
+    "url": "/csi-final/static/css/main.6562f555.chunk.css"
+  },
+  {
+    "revision": "a5596f52cb61df4d2bbb",
+    "url": "/csi-final/static/js/2.803c19a4.chunk.js"
+  },
+  {
+    "revision": "6dea14eab83053243865ab6f2019f727",
+    "url": "/csi-final/static/js/2.803c19a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fac74be35372251d4956",
+    "url": "/csi-final/static/js/main.71dfd2bb.chunk.js"
+  },
+  {
+    "revision": "2bc6df0a06eb144e3ceb",
+    "url": "/csi-final/static/js/runtime-main.76f955f5.js"
+  },
+  {
+    "revision": "d8d4d303eed5985a986d978c1b5b069b",
+    "url": "/csi-final/static/media/csi-logo.d8d4d303.png"
+  },
+  {
+    "revision": "e9b8a38152c08b04c8ec713385b14707",
+    "url": "/csi-final/static/media/svvv-logo.e9b8a381.png"
+  }
+]);
